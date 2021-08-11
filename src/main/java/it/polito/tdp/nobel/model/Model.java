66e3fb83e0c13627194294b3093b717553f7d8a9
Francesco Vittone <Francesco Vittone@LAPTOP-9K9S8ARF.homenet.telecomitalia.it>
2021-08-11 18:25:28 +0200

    pefezionamento no permutazioni

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -7,127 +7,110 @@ import java.util.Set;
 import it.polito.tdp.nobel.db.EsameDAO;
 
 public class Model {
-	
-	
+
 	private EsameDAO ed;
 	private List<Esame> tuttiEsami;
 	private int numeroCrediti;
 	private double mediaMigliore;
 	private Set<Esame> migliori;
-	
-	
+
 	public Model() {
 		this.ed = new EsameDAO();
 	}
 
-
 	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
-		
-		this.tuttiEsami= this.ed.getTuttiEsami();
-		this.numeroCrediti= numeroCrediti;
-		this.mediaMigliore=0;
+
+		this.tuttiEsami = this.ed.getTuttiEsami();
+		this.numeroCrediti = numeroCrediti;
+		this.mediaMigliore = 0;
 		this.migliori = new HashSet<Esame>();
 		this.permuta2(new HashSet<Esame>(), 0);
-		
-		if(this.migliori.size()>0)
+
+		if (this.migliori.size() > 0)
 			return this.migliori;
 		else
 			return null;
-		
+
 	}
-	
-	
-	private void permuta(Set <Esame> parziale, int livello) {
-		
-		if(this.sommaCrediti(parziale) == this.numeroCrediti) {
-			
+
+	private void permuta(Set<Esame> parziale, int livello) {
+
+		if (this.sommaCrediti(parziale) == this.numeroCrediti) {
+
 			double media = this.calcolaMedia(parziale);
-			
-			if(media>this.mediaMigliore)
-			{
-				this.mediaMigliore= media;
-				this.migliori= new HashSet<Esame>(parziale);
+
+			if (media > this.mediaMigliore) {
+				this.mediaMigliore = media;
+				this.migliori = new HashSet<Esame>(parziale);
 			}
 			return;
-			
-		}
-		else if(this.sommaCrediti(parziale) > this.numeroCrediti)
+
+		} else if (this.sommaCrediti(parziale) > this.numeroCrediti)
 			return;
-		else if(livello==this.tuttiEsami.size())
+		else if (livello == this.tuttiEsami.size())
 			return;
-		
+
 		else {
-			
-			for(int i=0;i<this.tuttiEsami.size();i++)
-			{
-				if(!parziale.contains(this.tuttiEsami.get(i)) && i>=livello)
-				{
+
+			for (int i = 0; i < this.tuttiEsami.size(); i++) {
+				if (!parziale.contains(this.tuttiEsami.get(i)) && i >= livello) {
 					parziale.add(this.tuttiEsami.get(i));
-					permuta(parziale,livello+1);
+					permuta(parziale, livello + 1);
 					parziale.remove(this.tuttiEsami.get(i));
 				}
 			}
-			
-		}
-		
-	}
-	
-private void permuta2(Set <Esame> parziale, int livello) {
-	
-	 int somma= this.sommaCrediti(parziale);
-		
-		if( somma == this.numeroCrediti) {
-			
-			double media = this.calcolaMedia(parziale);
-			
-			if(media>this.mediaMigliore)
-			{
-				this.mediaMigliore= media;
-				this.migliori= new HashSet<Esame>(parziale);
-			}
-			return;
-			
-		}
-		else if(somma > this.numeroCrediti)
-			return;
-		else if(livello==this.tuttiEsami.size())
-			return;
-		
-		else {
-			
-				for(int i=livello;i<this.tuttiEsami.size();i++)
-				{
-						parziale.add(this.tuttiEsami.get(i));
-						permuta2(parziale,livello+1);
-						parziale.remove(this.tuttiEsami.get(i));
-						permuta2(parziale,livello+1);
-						
-				}
-			}
-			
-		}
-	
 
-	
+		}
+
+	}
+
+	private void permuta2(Set<Esame> parziale, int livello) {
+
+		int somma = this.sommaCrediti(parziale);
+
+		if (somma == this.numeroCrediti) {
+
+			double media = this.calcolaMedia(parziale);
+
+			if (media > this.mediaMigliore) {
+				this.mediaMigliore = media;
+				this.migliori = new HashSet<Esame>(parziale);
+			}
+			return;
+
+		} else if (somma > this.numeroCrediti)
+			return;
+		else if (livello == this.tuttiEsami.size())
+			return;
+
+		else {
+			parziale.add(this.tuttiEsami.get(livello));
+			permuta2(parziale, livello + 1);
+			parziale.remove(this.tuttiEsami.get(livello));
+			permuta2(parziale, livello + 1);
+		}
+
+	}
+
 	public double calcolaMedia(Set<Esame> esami) {
-		
+
 		double crediti = 0;
 		double somma = 0;
-		
-		for(Esame e : esami){
+
+		for (Esame e : esami) {
 			crediti += e.getCrediti();
 			somma += (e.getVoto() * e.getCrediti());
 		}
-		
-		return somma/crediti;
+
+		return somma / crediti;
 	}
-	
+
 	public int sommaCrediti(Set<Esame> esami) {
 		int somma = 0;
-		
-		for(Esame e : esami)
+
+		for (Esame e : esami)
 			somma += e.getCrediti();
-		
+
 		return somma;
 	}
 
